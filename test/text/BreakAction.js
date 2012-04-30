be498743929149d082ee74209460ced4ddd1399e
Bram Stein <b.l.stein@gmail.com>
2012-04-30 18:11:39 +0200

    Added unit test and did some refactoring.

---
+++ test/text/BreakAction.js
@@ -0,0 +1,63 @@
+goog.require('tpo.text.BreakAction');
+goog.require('tpo.text.Token');
+goog.require('tpo.text.Tokenizer');
+
+describe('tpo.text.BreakAction', function() {
+    var BreakAction = tpo.text.BreakAction,
+        Token = tpo.text.Token;
+
+    describe('#write', function(done) {
+        it('handle a single token', function(done) {
+            var breakAction = new BreakAction();
+
+            breakAction.data.add(function(p, t) {
+                expect(t.tokenClass).to.eql(Token.Class.WJ);
+                expect(t.breakAction).to.eql(BreakAction.Type.EXPLICIT);
+            });
+            breakAction.end.add(done);
+
+            breakAction.write(null, new Token(1, Token.Class.SP));
+            breakAction.close();
+        });
+
+        it('handle two tokens', function(done) {
+            var breakAction = new BreakAction(),
+                result = [];
+
+            breakAction.data.add(function(p, t) {
+                result.push(t);
+            });
+            breakAction.end.add(function() {
+                expect(result[0].breakAction).to.eql(BreakAction.Type.PROHIBITED);
+                expect(result[1].breakAction).to.eql(BreakAction.Type.EXPLICIT);
+                done();
+            });
+
+            breakAction.write(null, new Token(1, Token.Class.AL));
+            breakAction.write(null, new Token(2, Token.Class.AL));
+            breakAction.close();
+        });
+
+        it('handle three tokens (AL, SP, AL)', function(done) {
+            var breakAction = new BreakAction(),
+                result = [];
+
+            breakAction.data.add(function(p, t) {
+                result.push(t);
+            });
+            breakAction.end.add(function() {
+                expect(result[0].breakAction).to.eql(BreakAction.Type.PROHIBITED);
+                expect(result[1].breakAction).to.eql(BreakAction.Type.INDIRECT);
+                expect(result[2].breakAction).to.eql(BreakAction.Type.EXPLICIT);
+                done();
+            });
+
+            breakAction.write(null, new Token(1, Token.Class.AL));
+            breakAction.write(null, new Token(2, Token.Class.SP));
+            breakAction.write(null, new Token(3, Token.Class.AL));
+            breakAction.close();
+        });
+
+        // TODO: Test combining classes
+    });
+});
--- test/text/Tokenizer.js
+++ test/text/Tokenizer.js
@@ -1,25 +1,27 @@
 goog.require('tpo.text.Tokenizer');
+goog.require('tpo.text.Token');
 
 describe('tpo.text.Tokenizer', function() {
     var Tokenizer = tpo.text.Tokenizer,
+        Token = tpo.text.Token,
         // Simple fake language that makes it easier to test
         language = {
             tokens: {
             }
         };
-    
-    language.tokens[Tokenizer.Class.SP] = /\s/;
-    language.tokens[Tokenizer.Class.AL] = /[^\s]+/;
-    
+
+    language.tokens[Token.Class.SP] = /\s/;
+    language.tokens[Token.Class.AL] = /[^\s]+/;
+
     describe('#constructor', function() {
         var t = new Tokenizer(language);
         it('has the tokens and token keys', function() {
             expect(t.tokens).to.eql(language.tokens);
-            expect(t.tokenKeys).to.eql([ Tokenizer.Class.AL.toString(), Tokenizer.Class.SP.toString()]);
-        }); 
+            expect(t.tokenKeys).to.eql([Token.Class.AL.toString(), Token.Class.SP.toString()]);
+        });
     });
 
-    describe('#write', function() {        
+    describe('#write', function() {
         it('empty input', function(done) {
             var t = new Tokenizer(language);
 
@@ -32,38 +34,38 @@ describe('tpo.text.Tokenizer', function() {
             t.write(null, '');
             t.close();
         });
-        
+
         it('returns one token', function(done) {
             var t = new Tokenizer(language),
                 result = [];
             t.data.add(function(position, token) {
-                result.push([token.breakClass, token.value]);
+                result.push([token.tokenClass, token.value]);
             });
             t.end.add(function() {
                 expect(result.length).to.eql(1);
-                expect(result[0][0]).to.eql(Tokenizer.Class.SP);
+                expect(result[0][0]).to.eql(Token.Class.SP);
                 expect(result[0][1]).to.eql(' ');
                 done();
             });
             t.write(null, ' ');
             t.close();
         });
-        
+
         it('returns more tokens', function(done) {
             var t = new Tokenizer(language),
                 result = [];
             t.data.add(function(position, token) {
-                result.push([token.breakClass, token.value]);
+                result.push([token.tokenClass, token.value]);
             });
             t.end.add(function() {
                 expect(result.length).to.eql(3);
-                expect(result[0][0]).to.eql(Tokenizer.Class.AL);
+                expect(result[0][0]).to.eql(Token.Class.AL);
                 expect(result[0][1]).to.eql('some');
-                
-                expect(result[1][0]).to.eql(Tokenizer.Class.SP);
+
+                expect(result[1][0]).to.eql(Token.Class.SP);
                 expect(result[1][1]).to.eql(' ');
-                
-                expect(result[2][0]).to.eql(Tokenizer.Class.AL);
+
+                expect(result[2][0]).to.eql(Token.Class.AL);
                 expect(result[2][1]).to.eql('text');
                 done();
             });
