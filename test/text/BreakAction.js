496f743e25a784887f0a0b0c86bd647d29fc5de9
Bram Stein <b.l.stein@gmail.com>
2012-07-11 21:06:43 +0200

    Fixed break action tests.

--- test/text/BreakAction.js
+++ test/text/BreakAction.js
@@ -7,46 +7,53 @@ describe('tpo.text.BreakAction', function() {
         Token = tpo.text.Token;
 
     describe('#write', function(done) {
+        it('handles empty input', function() {
+            var breakAction = new BreakAction();
+
+            var actions = breakAction.find([]);
+
+            expect(actions.length).to.eql(0);
+        });
+
         it('handle a single token', function() {
             var breakAction = new BreakAction(),
                 tokens = [new Token(1, Token.Class.SP)];
 
-            breakAction.find(tokens);
+            var actions = breakAction.find(tokens);
 
-            expect(tokens[0].tokenClass).to.eql(Token.Class.SP);
-            expect(tokens[0].breakAction).to.eql(BreakAction.Type.EXPLICIT);
+            expect(actions[0]).to.eql(BreakAction.Type.EXPLICIT);
         });
 
         it('handle two tokens', function() {
             var breakAction = new BreakAction(),
                 tokens = [new Token(1, Token.Class.AL), new Token(2, Token.Class.AL)];
 
-            breakAction.find(tokens);
+            var actions = breakAction.find(tokens);
 
-            expect(tokens[0].breakAction).to.eql(BreakAction.Type.PROHIBITED);
-            expect(tokens[1].breakAction).to.eql(BreakAction.Type.EXPLICIT);
+            expect(actions[0]).to.eql(BreakAction.Type.PROHIBITED);
+            expect(actions[1]).to.eql(BreakAction.Type.EXPLICIT);
         });
 
         it('handle indirect breaks (AL, SP, AL)', function() {
             var breakAction = new BreakAction(),
                 tokens = [new Token(1, Token.Class.AL), new Token(2, Token.Class.SP), new Token(3, Token.Class.AL)];
 
-            breakAction.find(tokens);
+            var actions = breakAction.find(tokens);
 
-            expect(tokens[0].breakAction).to.eql(BreakAction.Type.PROHIBITED);
-            expect(tokens[1].breakAction).to.eql(BreakAction.Type.INDIRECT);
-            expect(tokens[2].breakAction).to.eql(BreakAction.Type.EXPLICIT);
+            expect(actions[0]).to.eql(BreakAction.Type.PROHIBITED);
+            expect(actions[1]).to.eql(BreakAction.Type.INDIRECT);
+            expect(actions[2]).to.eql(BreakAction.Type.EXPLICIT);
         });
 
         it('handles direct breaks (AL, B2, AL)', function() {
             var breakAction = new BreakAction(),
                 tokens = [new Token(1, Token.Class.AL), new Token(2, Token.Class.B2), new Token(3, Token.Class.AL)];
 
-            breakAction.find(tokens);
+            var actions = breakAction.find(tokens);
 
-            expect(tokens[0].breakAction).to.eql(BreakAction.Type.DIRECT);
-            expect(tokens[1].breakAction).to.eql(BreakAction.Type.DIRECT);
-            expect(tokens[2].breakAction).to.eql(BreakAction.Type.EXPLICIT);
+            expect(actions[0]).to.eql(BreakAction.Type.DIRECT);
+            expect(actions[1]).to.eql(BreakAction.Type.DIRECT);
+            expect(actions[2]).to.eql(BreakAction.Type.EXPLICIT);
         });
 
         it('handles explicit breaks (AL, NL, AL, AL)', function() {
@@ -58,12 +65,12 @@ describe('tpo.text.BreakAction', function() {
                     new Token(4, Token.Class.AL)
                 ];
 
-            breakAction.find(tokens);
+            var actions = breakAction.find(tokens);
 
-            expect(tokens[0].breakAction).to.eql(BreakAction.Type.PROHIBITED);
-            expect(tokens[1].breakAction).to.eql(BreakAction.Type.EXPLICIT);
-            expect(tokens[2].breakAction).to.eql(BreakAction.Type.PROHIBITED);
-            expect(tokens[3].breakAction).to.eql(BreakAction.Type.EXPLICIT);
+            expect(actions[0]).to.eql(BreakAction.Type.PROHIBITED);
+            expect(actions[1]).to.eql(BreakAction.Type.EXPLICIT);
+            expect(actions[2]).to.eql(BreakAction.Type.PROHIBITED);
+            expect(actions[3]).to.eql(BreakAction.Type.EXPLICIT);
         });
 
         it('handles prohibited breaks (AL, AL, AL)', function() {
@@ -74,11 +81,11 @@ describe('tpo.text.BreakAction', function() {
                     new Token(3, Token.Class.AL)
                 ];
 
-            breakAction.find(tokens);
+            var actions = breakAction.find(tokens);
 
-            expect(tokens[0].breakAction).to.eql(BreakAction.Type.PROHIBITED);
-            expect(tokens[1].breakAction).to.eql(BreakAction.Type.PROHIBITED);
-            expect(tokens[2].breakAction).to.eql(BreakAction.Type.EXPLICIT);
+            expect(actions[0]).to.eql(BreakAction.Type.PROHIBITED);
+            expect(actions[1]).to.eql(BreakAction.Type.PROHIBITED);
+            expect(actions[2]).to.eql(BreakAction.Type.EXPLICIT);
         });
 
         it('handles soft hyphens (AL, BA, AL, AL)', function() {
@@ -90,14 +97,50 @@ describe('tpo.text.BreakAction', function() {
                     new Token(4, Token.Class.AL)
                 ];
 
-            breakAction.find(tokens);
+            var actions = breakAction.find(tokens);
 
-            expect(tokens[0].breakAction).to.eql(BreakAction.Type.PROHIBITED);
-            expect(tokens[1].breakAction).to.eql(BreakAction.Type.DIRECT);
-            expect(tokens[2].breakAction).to.eql(BreakAction.Type.PROHIBITED);
-            expect(tokens[3].breakAction).to.eql(BreakAction.Type.EXPLICIT);
+            expect(actions[0]).to.eql(BreakAction.Type.PROHIBITED);
+            expect(actions[1]).to.eql(BreakAction.Type.DIRECT);
+            expect(actions[2]).to.eql(BreakAction.Type.PROHIBITED);
+            expect(actions[3]).to.eql(BreakAction.Type.EXPLICIT);
         });
 
+        it('handles question marks (AL, EX, SP, AL, AL)', function() {
+            var breakAction = new BreakAction(),
+                tokens = [
+                    new Token(1, Token.Class.AL),
+                    new Token(2, Token.Class.EX),
+                    new Token(3, Token.Class.SP),
+                    new Token(4, Token.Class.AL),
+                    new Token(5, Token.Class.AL)
+                ];
+
+            var actions = breakAction.find(tokens);
+
+            expect(actions[0]).to.eql(BreakAction.Type.PROHIBITED);
+            expect(actions[1]).to.eql(BreakAction.Type.PROHIBITED);
+            expect(actions[2]).to.eql(BreakAction.Type.DIRECT);
+            expect(actions[3]).to.eql(BreakAction.Type.PROHIBITED);
+            expect(actions[4]).to.eql(BreakAction.Type.EXPLICIT);
+        });
+
+        it('handles em dashes (AL, B2, AL, AL)', function() {
+            var breakAction = new BreakAction(),
+                tokens = [
+                    new Token(1, Token.Class.AL),
+                    new Token(2, Token.Class.B2),
+                    new Token(3, Token.Class.AL),
+                    new Token(4, Token.Class.AL)
+                ];
+
+            var actions = breakAction.find(tokens);
+
+            expect(actions[0]).to.eql(BreakAction.Type.DIRECT);
+            expect(actions[2]).to.eql(BreakAction.Type.DIRECT);
+            expect(actions[3]).to.eql(BreakAction.Type.PROHIBITED);
+            expect(actions[4]).to.eql(BreakAction.Type.EXPLICIT);
+
+        });
         // TODO: Test combining classes
     });
 });
